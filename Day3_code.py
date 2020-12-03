b71dd70a08fb5a70677237295f596a22996d56b2
asyaparfenova <asya.parfenova@gmail.com>
2020-12-03 13:13:42 +0100

    Day3_code is modified, so it solves the 2 nd task as well

--- Day3_code.py
+++ Day3_code.py
@@ -10,26 +10,34 @@ import re
 import pyperclip
 
 data = pyperclip.paste()
+steps_set = [(1,1), (3,1), (5,1), (7,1), (1,2)]
 
-def make_map(data):
+def get_listed_map(data: str):
+    '''breaks the input into list of lines'''
     line_re = r'[.|#]+'
-    input_lines = re.findall(line_re, data)
-    dev = len(input_lines) // len(input_lines[0])
-    map = []
-    for line in input_lines:
-        map.append(line * 3 * (dev + 1))
-    return map
+    listed_map = re.findall(line_re, data)
+    return listed_map
 
-def count_trees(map):
+def count_trees(map: str, steps: tuple):
+    '''counts encountered trees on given map with given steps pattern (x,y)'''
     x = 0
     trees = 0
-    for row in map:
+    for row in map[::steps[1]]:
         if row[x] == '#':
             trees += 1
-        x += 3
+        x = (x + steps[0]) % len(row)
     return trees
 
 if __name__ == "__main__":
-    forest = make_map(data)
-    trees = count_trees(forest)
-    print('You incountered', trees, 'trees :/')
+    
+    steps_product = 1
+    
+    for steps in steps_set:
+        forest = get_listed_map(data)
+        trees = count_trees(forest, steps)
+        
+        steps_product *= trees
+        
+        print(f'Following a slope of right {steps[0]} and down {steps[1]}, you will encounter {trees} trees')
+    print(f'/nAll trees multiplied are {steps_product}')
+        
