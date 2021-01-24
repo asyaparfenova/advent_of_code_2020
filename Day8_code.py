7cbe2731eeadb871cfd0bdfdde214eabbab6135e
asyaparfenova <asya.parfenova@gmail.com>
2021-01-24 14:32:48 +0100

    cleaned Day8 code

--- Day8_code.py
+++ Day8_code.py
@@ -1,130 +1,98 @@
 '''This programm is a solution
 for day 8 challenges of Advent of Code 2020:
-Handheld Halting.
+Handheld Halting
 
 Before running this code you should copy your
 input data into the clipboard'''
 
 import copy
-
 import pyperclip
 
 data = pyperclip.paste()
 
-
-def make_rule_list(data):
-    #print(data[:5])
-    rules = data.split('\r\n')
-    rule_list = []
-    for rule in rules:
-        sep = rule.split(' ')
-        rule_list.append([sep[0], sep[1], 0])
-    return rule_list
-
-
-def play_game(start_pos, start_score, data):
-    rule_list = make_rule_list(data)
-    pos = start_pos
-    score = start_score
-    while pos < len(rule_list) and rule_list[pos][2] != 1:
+def clean_commands(data):
+    '''
+    Parameters
+    ----------
+    commands : string
         
-        if rule_list[pos][0] == 'nop':
-            #print(pos, score)
-            rule_list[pos][2] += 1
-            pos += 1
-        elif rule_list[pos][0] == 'jmp':
-            #print(pos, score)
-            rule_list[pos][2] += 1
-            if rule_list[pos][1][:1] == '+':
-                pos += int(rule_list[pos][1][1:])
-            else:
-                pos -= int(rule_list[pos][1][1:])
+    Returns
+    -------
+    steps : list (of lists)
+    '''
+    commands = data.split('\r\n')
+    steps = []
+    for command in commands:
+        instruction = command[0:3]
+        if command[4] == '+':
+            step_num = int(command[5:])
         else:
-            #print(pos, score)
-            rule_list[pos][2] += 1
-            if rule_list[pos][1][:1] == '+':
-                score += int(rule_list[pos][1][1:])
-                pos += 1
-            else:
-                score -= int(rule_list[pos][1][1:])
-                pos += 1
-    if pos >= len(rule_list):
-        print(score)
-        print('out')
-        return ('out', score)
-    else:
-        #print('loop')
-        return ('loop', score)
-    
-    
-def fix_bug(data):
-    rule_list = make_rule_list(data)
-    print(rule_list[:5])
-    for i in range(len(rule_list)):
-        new_list = copy.deepcopy(rule_list)
-        if new_list[i][0] == 'nop':
-            new_list[i][0] = 'jmp'
-            print("tryed",i, new_list[i])
-            print(play_game(0,0,data))
-            if play_game(0,0,data)[0] == 'out':
-                print('out')
-                return (i, play_game(0,0,new_list)[1])
-        elif new_list[i][0] == 'jmp':
-            new_list[i][0] = 'nop'
-            print("tryed",i, new_list[i])
-            print(play_game(0,0,data))
-            if play_game(0,0,data)[0] == 'out':
-                print('out')
-                return (i, play_game(0,0,new_list)[1])
-            
-def game_two(start_pos, start_score, rule_list):
-    pos = start_pos
-    score = start_score
-    while pos < len(rule_list) and rule_list[pos][2] != 1:
-        
-        if rule_list[pos][0] == 'nop':
-            #print(pos, score)
-            rule_list[pos][2] += 1
-            pos += 1
-        elif rule_list[pos][0] == 'jmp':
-            #print(pos, score)
-            rule_list[pos][2] += 1
-            if rule_list[pos][1][:1] == '+':
-                pos += int(rule_list[pos][1][1:])
-            else:
-                pos -= int(rule_list[pos][1][1:])
-        else:
-            #print(pos, score)
-            rule_list[pos][2] += 1
-            if rule_list[pos][1][:1] == '+':
-                score += int(rule_list[pos][1][1:])
-                pos += 1
-            else:
-                score -= int(rule_list[pos][1][1:])
-                pos += 1
-    if pos >= len(rule_list):
-        print(score)
-        return ('out', score)
-    else:
-        #print('loop')
-        return ('loop', score)
-            
-if __name__ == '__main__':    
-    print(play_game(0, 0, data))
-    rule_list = make_rule_list(data)
-    for i in range(len(rule_list)):
-        new_list = copy.deepcopy(rule_list)
-        if new_list[i][0] == 'nop':
-            new_list[i][0] = 'jmp'
-            #print("tryed",i, new_list[i])
-            #print(game_two(0,0,new_list))
-            if game_two(0,0,new_list)[0] == 'out':
-                break
-        elif new_list[i][0] == 'jmp':
-            new_list[i][0] = 'nop'
-            #print("tryed",i, new_list[i])
-            answer = game_two(0,0,new_list)
-            if answer[0] == 'out':
-                print('ended with', i, answer[1])
-                break
+            step_num = - int(command[5:])
+        steps.append([instruction, step_num])
+    return steps
 
+def play_game(steps):
+    '''
+    Follows the game command until the first loop
+    Parameters
+    ----------
+    steps : TYPE
+        list of commands - each of them is a list 
+
+    Returns
+    -------
+    list
+        two values - game result and the first step that was not executed
+    '''
+    game = [0]*len(steps)
+    i = 0
+    game_sum = 0
+    while i < len(game) and game[i] == 0:
+        if steps[i][0] == 'nop':
+            game[i] = 1
+            i += 1
+        elif steps[i][0] == 'acc':
+            game_sum += steps[i][1]
+            game[i] = 1
+            i += 1
+        else:
+            game[i] = 1
+            i += steps[i][1]
+    return [game_sum, i]
+
+def fix_game(steps):
+    '''
+    Tries to fix the game step by step until success
+    Parameters
+    ----------
+    steps : list
+        initial list of commands - each of them is a list 
+
+    Returns
+    -------
+    int
+        result of the fixed game
+
+    '''
+    for i in range(len(steps)):
+        if steps[i][0] == 'nop':
+            steps_deepcopy = copy.deepcopy(steps)
+            steps_deepcopy[i][0] = 'jmp'
+            last_step = play_game(steps_deepcopy)[1]
+            if last_step >= len(steps):
+                return play_game(steps_deepcopy)
+        elif steps[i][0] == 'jmp':
+            steps_deepcopy = copy.deepcopy(steps)
+            steps_deepcopy[i][0] = 'nop'
+            last_step = play_game(steps_deepcopy)[1]
+            if last_step >= len(steps):
+                return play_game(steps_deepcopy)[0]
+
+
+if __name__ == '__main__':
+    steps = clean_commands(data)
+    print(play_game(steps)[0])
+    print(fix_game(steps))
+
+
+    
