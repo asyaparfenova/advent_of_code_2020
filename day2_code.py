f2bef92f7464602b1536456cd5455224433448c8
asyaparfenova <asya.parfenova@gmail.com>
2021-02-13 18:39:47 +0100

    edit style

--- day2_code.py
+++ day2_code.py
@@ -1,18 +1,17 @@
 '''This programm calculates the answer
-for day 2 challenges of Advent of Code 2020.
-
-Before running this code you should copy your
-input data into the clipboard'''
+for day 2 challenges of Advent of Code 2020'''
 
 import re
-import pyperclip
 
-data = pyperclip.paste()
 
-line_re = r'(\d.+)'
-input_lines = re.findall(line_re, data)
+def get_input_lines(input_data):
+    '''extraact lines from input text'''
+    line_re = r'(\d.+)'
+    input_lines = re.findall(line_re, input_data)
+    return input_lines
 
-def verify_password_old(line: str):
+
+def verify_password_old(input_line: str):
     '''
        Verifies if the password is valid in each policy+password line
        according to the old policies (Task 1)
@@ -22,18 +21,18 @@ def verify_password_old(line: str):
     max_re = r'-([\d]+) '
     letter_re = r' ([a-z]):'
     password_re = r'[a-z][a-z]+'
-    min_value = int(re.findall(min_re, line)[0])
-    max_value = int(re.findall(max_re, line)[0])
-    letter = re.findall(letter_re, line)[0]
-    password = re.findall(password_re, line)[0]
+    min_value = int(re.findall(min_re, input_line)[0])
+    max_value = int(re.findall(max_re, input_line)[0])
+    letter = re.findall(letter_re, input_line)[0]
+    password = re.findall(password_re, input_line)[0]
     rule_re = fr'{letter}'
     result = re.findall(rule_re, password)
     if len(result) >= min_value and len(result) <= max_value:
         return 1
-    else:
-        return 0
-    
-def verify_password_new(line):
+    return 0
+
+
+def verify_password_new(input_line):
     '''
        Verifies if the password is valid in each policy+password line
        according to the new policies (Task 2)
@@ -43,24 +42,25 @@ def verify_password_new(line):
     position_two_re = r'-([\d]+) '
     letter_re = r' ([a-z]):'
     password_re = r'[a-z][a-z]+'
-    position_one = int(re.findall(position_one_re, line)[0]) - 1
-    position_two = int(re.findall(position_two_re, line)[0]) - 1
-    letter = re.findall(letter_re, line)[0]
-    password = re.findall(password_re, line)[0]
-    if password[position_one] == letter and password[position_two] == letter:
+    position_one = int(re.findall(position_one_re, input_line)[0]) - 1
+    position_two = int(re.findall(position_two_re, input_line)[0]) - 1
+    letter = re.findall(letter_re, input_line)[0]
+    password = re.findall(password_re, input_line)[0]
+    if password[position_one] == letter and password[position_two] == letter or password[position_one] != letter and password[position_two] != letter:
         return 0
-    elif password[position_one] != letter and password[position_two] != letter:
-        return 0
-    else:
-        return 1    
-    
-if __name__ == "__main__":
-    good_passwords_old = 0
-    good_passwords_new = 0
-    for line in input_lines:
-        good_passwords_old += verify_password_old(line)
-        good_passwords_new += verify_password_new(line)
-    print('According to the old policies we have', good_passwords_old, 'valid passwords')
-    print('According to the new policies we have', good_passwords_new, 'valid passwords')
-    
+    return 1
 
+
+if __name__ == "__main__":
+    with open('day_2_passwords.txt','r') as file:
+        data = file.read()
+    lines = get_input_lines(data)
+    GOOD_PASSWORDS_OLD = 0
+    GOOD_PASSWORDS_NEW = 0
+
+    for line in lines:
+        GOOD_PASSWORDS_OLD += verify_password_old(line)
+        GOOD_PASSWORDS_NEW += verify_password_new(line)
+
+    print('According to the old policies we have', GOOD_PASSWORDS_OLD, 'valid passwords')
+    print('According to the new policies we have', GOOD_PASSWORDS_NEW, 'valid passwords')
